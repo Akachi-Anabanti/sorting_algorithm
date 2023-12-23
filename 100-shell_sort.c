9385a72427c8165e6f5e51ee4b9da55aedb375b6
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-23 22:15:52 +0000

    shell sort algorithm

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * shell_sort - shell sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+	int temp;
+
+	if (!array || size == 0)
+		return;
+	gap = 0;
+	while (gap < size)
+		gap = gap * 3 + 1;
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = temp;
+		}
+		gap = gap / 3;
+		print_array(array, size);
+	}
+}
