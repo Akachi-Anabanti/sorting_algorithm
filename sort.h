6396aff2c31d8fddcdf56422e9dc76f721a153a1
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-23 22:37:48 +0000

    updated header file

--- sort.h
+++ sort.h
@@ -22,6 +22,11 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+
+/*helper function*/
+void swap(int *, int *);
+void sorter(int *array, int, int, size_t);
+int partitioner(int *array, int, int, size_t);
 /*task function prototypes*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
