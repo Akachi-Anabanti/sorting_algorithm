a67f18580b74ea203b0864abe5000cd333167477
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-31 18:17:11 +0000

    refactor

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,5 +1,25 @@
 #include "sort.h"
 #include <limits.h>
+
+
+/**
+ * max - returns the max value of an array of size size
+ *
+ * @array: The array
+ * @size: The size of the array
+ * Return: The maximum value
+ */
+int max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
+}
 /**
  * counting_sort - Counting sort algorithm
  *
@@ -8,41 +28,38 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int *count, *output, k = array[0], min = array[0];
-	size_t i, j;
+	int *count, *output, k, i;
 
-	for (i = 0; i < size; i++)
-	{
-		if (array[i] > k)
-			k = array[i];
-		if (array[i] < min)
-			min = array[i];
-	}
-	for (i = 0; i < size; i++)
-		array[i] -= min;
-	k -= min;
-	count = malloc(sizeof(int) * (k + 1));
-	output = malloc(sizeof(int) * size);
-	if (count == NULL || output == NULL)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < (size_t)k + 1; i++)
-		count[i] = 0;
-	for (j = 0; j < size; ++j)
+	k = max(array, size);
+	output = malloc(sizeof(int) * size);
+	if (output == NULL)
+		return;
+	count = malloc(sizeof(int) * (k + 1));
+	if (count == NULL)
 	{
-		++count[array[j]];
+		free(output);
+		return;
 	}
-	for (i = 1; i <= (size_t)k; ++i)
+	/**assign the values of the array to zero**/
+	for (i = 0; i < (k + 1); i++)
+		count[i] = 0;
+	/* assing the index value of count**/
+	for (i = 0; i < (int)size; i++)
+	{
+		count[array[i]] += 1;
+	}
+	for (i = 0; i < (k + 1); i++)
 		count[i] += count[i - 1];
 	print_array(count, k + 1);
-	for (i = 0; i < size; ++i)
+	for (i = 0; i < (int)size; i++)
 	{
 		output[count[array[i]] - 1] = array[i];
-		--count[array[i]];
+		count[array[i]] -= 1;
 	}
-	for (i = 0; i < size; ++i)
+	for (i = 0; i < (int)size; ++i)
 		array[i] = output[i];
-	for (i = 0; i < size; ++i)
-		array[i] += min;
 	free(count);
 	free(output);
 }
