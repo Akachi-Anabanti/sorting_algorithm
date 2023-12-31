6302e2d73a335f6c13cef4261fd9026608a5f6ef
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-31 18:35:45 +0000

    fixed betty error

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -50,7 +50,7 @@ void counting_sort(int *array, size_t size)
 	{
 		count[array[i]] += 1;
 	}
-	for (i= 1; i < (k + 1); i++)
+	for (i = 1; i < (k + 1); i++)
 		count[i] += count[i - 1];
 	print_array(count, k + 1);
 	for (i = 0; i < (int)size; i++)
